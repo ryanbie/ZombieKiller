e2939b62a087e2518f7b36ece07089faa5f35cf3
Ryan Bie <30966505+ryanbie@users.noreply.github.com>
2021-10-21 18:15:03 -0400

    Create ZombieKiller.java

---
+++ ZombieKiller.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to our zombie killer program!");
+
+        String backpack[] = { "Shotgun" , "Assault Rifle" , "Sniper Rifle" };
+        String zombies[] = {"Close-range zombie", "Mid-range zombie", "Long-range zombie"};
+
+        System.out.println("Backpack items:");
+        System.out.println(backpack[0]);
+        System.out.println(backpack[1]);
+        System.out.println(backpack[2]);
+
+        System.out.println("These are the zombies:");
+        System.out.println(zombies[0]);
+        System.out.println(zombies[1]);
+        System.out.println(zombies[2]);
+    }
+}
